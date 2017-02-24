efe7b78c79516bb0a4a1bff32f11596dbc14977b
Mike McQueen <mike.mcqueen@yahoo.com>
2017-02-23 23:30:26 -0800

    filter.js now (ostensibly) checks _filtered.json

--- src/crawl/filter.js
+++ src/crawl/filter.js
@@ -8,6 +8,8 @@ const _            = require('lodash');
 const Promise      = require('bluebird');
 const Dir          = require('node-dir');
 const Path         = require('path');
+const FS           = require('fs');
+const fsReadFile   = Promise.promisify(FS.readFile);
 const expect       = require('chai').expect;
 
 const ClueManager  = require('../clue_manager.js');
@@ -27,7 +29,8 @@ const Opt          = require('node-getopt').create([
 
 //
 
-const RESULT_DIR = '../../data/results/';
+const RESULT_DIR =      '../../data/results/';
+const FILTERED_SUFFIX = '_filtered';
 
 //
 
@@ -41,7 +44,9 @@ function getUrlCount(resultList) {
 
 //
 
-function makeFilename(wordList) {
+function makeFilename(wordList, suffix) {
+    expect(wordList.length).to.be.at.least(2);
+
     let filename = '';
     wordList.forEach(word => {
 	if (_.size(filename) > 0) {
@@ -49,12 +54,15 @@ function makeFilename(wordList) {
 	}
 	filename += word;
     });
+    if (!_.isUndefined(suffix)) {
+	filename += suffix;
+    }
     return filename + '.json';
 }
 
 //
 
-function filterSearchResult(wordCount, result, options) {
+function searchResultWordCountFilter(result, wordCount, options) {
     return new Promise(function(resolve, reject) {
 	let passTitle = false;
 	let passArticle = false;
@@ -71,22 +79,38 @@ function filterSearchResult(wordCount, result, options) {
 		resolve(result);
 	    }
 	}
-	resolve(null);
+	resolve(undefined);
     });
 }
 
 //
 
-function filterSearchResultList(wordList, resultList, options) {
+function isFilteredUrl(url, filteredUrls) {
+    if (_.isUndefined(filteredUrls)) return false; 
+    let reject = !_.isUndefined(filteredUrls.rejectUrls) &&
+	filteredUrls.rejectUrls.includes(url);
+    let known = !_.isUndefined(filteredUrls.knownUrls) &&
+	filteredUrls.knownUrls.includes(url);
+    return known || reject;
+}
+
+
+//
+
+function filterSearchResultList(resultList, wordList, filteredUrls, options) {
     return new Promise((resolve, reject) => {
 	let urlList = [];
+	// make any clue words with a space into multiple words.
 	let wordCount = _.chain(wordList).map(word => word.split(' ')).flatten().size().value();
 	Promise.map(resultList, result => {
-	    return filterSearchResult(wordCount, result, options);
+	    if (!isFilteredUrl(result.url, filteredUrls)) {
+		return searchResultWordCountFilter(result, wordCount, options);
+	    }
+	    return undefined;
 	}).each((result, index) => {
-	    if (result !== null) {
-		urlList.push(result.url);
+	    if (!_.isUndefined(result)) {
 		//console.log('url: ' + result.url);
+		urlList.push(result.url);
 	    }
 	}).then(filteredList => {
 	    //console.log('urlList.size = ' + _.size(urlList));
@@ -101,6 +125,30 @@ function filterSearchResultList(wordList, resultList, options) {
 
 //
 
+function loadFilteredUrls(dir, wordList) {
+    return new Promise((resolve, reject) => {
+	let filename = makeFilename(wordList, FILTERED_SUFFIX);
+	return fsReadFile(filename, 'utf8')
+	    .then(content => {
+		// TODO: what happens on JSON.parse throw?
+		resolve(JSON.parse(content));
+	    }).catch(err => {
+		// ignore file errors, this file is optional
+		resolve(undefined);
+	    });
+    });
+}
+
+// for each search result filename in dir that matches fileMatch
+//   filter out rejected word combinations
+//   load the file, build filtered URL list
+//   load the _filtered.json file, filter out known/reject URLs
+//   if any URLs remain, add result to filteredList.
+//
+// NOTE that for convenience, this function currently loads all
+// files that match fileMatch, even if the file's word-combo is
+// a rejected combo. 
+// 
 function filterSearchResultFiles(dir, fileMatch, options) {
     return new Promise((resolve, reject) => {
 	let filteredList = [];
@@ -110,29 +158,34 @@ function filterSearchResultFiles(dir, fileMatch, options) {
 	    exclude: /^\./,
 	    recursive: false
 	}, function(err, content, filepath, next) {
+	    if (options.verbose) {
+		console.log(`filename: ${filepath}`);
+	    }
 	    if (err) throw err;
-	    //console.log('filename: ' + filepath);
-	    // TODO: what happens on JSON.parse throw?
-	    filterSearchResultList(
-		_.split(Path.basename(filepath, '.json'), '-'),
-		JSON.parse(content),
-		options
-	    ).catch(err => {
-		console.error('filterResultList error: ' + err.message);
-	    }).then(result => {
-		if (!_.isEmpty(result.urlList)) {
-		    filteredList.push(result);
-		}
-		else {
-		    rejectList.push(result);
-		}
-		return next();
-	    });
+	    let wordList = _.split(Path.basename(filepath, '.json'), '-');
+	    // filter out rejected word combos
+	    if (ClueManager.isRejectSource(wordList)) next();
+
+	    loadFilteredUrls(dir, wordList)
+		.then(filteredUrls => {
+		    // TODO: what happens on JSON.parse throw?
+		    return filterSearchResultList(JSON.parse(content), wordList, filteredUrls, options);
+		}).then(filterResult => {
+		    if (!_.isEmpty(filterResult.urlList)) {
+			filteredList.push(filterResult);
+		    }
+		    else {
+			rejectList.push(filterResult);
+		    }
+		    return next();
+		}).catch(err => {
+		    console.error('filterSearchResultFiles, error: ' + err.message);
+		});
 	}, function(err, files) {
             if (err) throw err;
 	    resolve({
 		filtered: filteredList,
-		rejects:  rejectList
+		rejects: rejectList
 	    });
 	});
     });
@@ -200,7 +253,9 @@ function main() {
 	    else {
 		displayFilterResults(Opt.options.rejects ? result.rejects : result.filtered);
 	    }
-	}).catch(err => console.error);
+	}).catch(err => {
+	    console.error(err);
+	});
 }
 
 //
