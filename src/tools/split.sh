e326309430a95f359d21c523936f648ac6d035c2
Mike McQueen <mike.mcqueen@yahoo.com>
2017-11-09 00:38:06 -0800

    split.sh

---
+++ src/tools/split.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+if [ $# -lt 1  ]
+then
+   echo 'usage: split.sh <filename> [chunks]'
+   echo '  chunks defaults to 3'
+   exit -1
+fi
+
+_filename=$1  #filename
+shift
+
+_chunks=3
+if [[ $# -gt 0 ]]
+then
+   _chunks=$1
+   shift
+fi
+
+_wcl=$(wc -l $_filename) # line count as ugly string
+_lines=$(echo $_wcl | sed -E 's/^[^[[:digit:]]*([[:digit:]]+).*$/\1/') # line count
+
+_lpc=$(($_lines / $_chunks)) # lines per chunk
+
+echo "lines: $_lines, chunks: $_chunks, lpc: $_lpc"
+
+split $filename tmp/$filename.
