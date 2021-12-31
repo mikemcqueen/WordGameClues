fd77c2c07accb0117e4e2c354397df156eb72fd8
mike <handmeat@yahoo.com>
2021-12-31 14:42:20 -0800

    para.js

--- src/tools/para.js
+++ src/tools/para.js
@@ -1,7 +1,23 @@
 'use strict';
 
-exports = ParaFunc;
+let Parallel = require('paralleljs');
+let ComboMakerWorkerBootstrap = require('../modules/combo-maker-worker-bootstrap');
 
-function ParaFunc(data) {
-    console.log(`ParaFunc: ${data}`);
+function test(data) {
+    console.log(`test: ${data}`);
+    return 0;
 }
+
+async function main() {
+    let data = [0, 1, 2, 3];
+    let p = new Parallel(data, {
+	//evalPath: 'https://raw.github.com/parallel-js/parallel.js/master/lib/eval.js'
+	evalPath: '${__dirname}/../../modules/combo-maker-worker-bootstrap.js'
+    });
+    
+    console.log('begin');
+    p.map(ComboMakerWorkerBootstrap.entrypoint).then(_ => { console.log('end'); });
+    //p.map(test).then(_ => { console.log('end'); });
+}
+
+main().catch(e => { console.error(e); });
