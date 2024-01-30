60ce34b35509abf1cb82b2ef74b3c3dd7edf3246
Mike McQueen <handmeat@yahoo.com>
2024-01-30 11:59:19 -0800

    working show-solutions (i think)

--- src/cm/folder.ts
+++ src/cm/folder.ts
@@ -14,11 +14,24 @@ export const file_exists = (dir: string, file: string): boolean => {
     return Fs.pathExistsSync(make_path(dir, file));
 };
 
+
+export const find_file_up = (dir: string, filename: string): string => {
+    // because logic is slightly simpler if true
+    Assert((dir.length > 0) && (dir[0] === '/'), 'absolute path required');
+    while (!file_exists(dir, filename)) {
+        Assert(dir.length > 1, `${filename} not found`);
+        const last_index = dir.lastIndexOf('/');
+        dir = dir.slice(0, last_index);
+    }
+    return make_path(dir, filename);;
+};
+
 export const find_root = (dir: string): string => {
+//    return find_file_up(dir, ROOT_FILE);
     // because logic is slightly simpler if true
     Assert((dir.length > 0) && (dir[0] === '/'), 'absolute path required');
     while (!file_exists(dir, ROOT_FILE)) {
-        console.error(dir);
+        //console.error(dir);
         Assert(dir.length > 1, 'root not found');
         const last_index = dir.lastIndexOf('/');
         dir = dir.slice(0, last_index);
--- src/cm/solutions.ts
+++ src/cm/solutions.ts
@@ -1,26 +1,98 @@
 import * as Folder from './folder';
 import * as Json from './json';
+import * as enhanceJSON from 'enhancejson';
 const Assert = require('assert');
 const Fs = require('fs-extra');
 //const Stringify = require('stringify-object');
 
-export const get_all = (starting_dir: string = process.cwd(), show = false): string[] => {
+type MapEntry = {
+  [key: string]: string[] | Set<string>;
+};
+type MapType = Map<string, MapEntry>;
+
+type FileMapEntry = Record<string, string[]>;
+type FileMapType = Record<string, FileMapEntry>;
+
+export const get_all = (starting_dir: string = process.cwd()): Map<string, string[]> => {
     const root_dir = Folder.find_root(starting_dir);
     const child_dirs = starting_dir.slice(root_dir.length).split('/').filter(dir => dir.length);
-    if (show) console.error(`root_dir: ${root_dir} child_dirs: ${child_dirs.toString()}`);
-    let all_solutions: string[] = [];
+    //if (show) console.error(`root_dir: ${root_dir} child_dirs: ${child_dirs.toString()}`);
+    let result = new Map<string, string[]>();
     let dir = Folder.make_path(root_dir.slice(), child_dirs[0]);
     // skip first level below root (no solutions)
     for (let idx = 1; idx < child_dirs.length; ++idx) {
-        dir = Folder.make_path(dir, child_dirs[idx]);
+        const child = child_dirs[idx];
+        dir = Folder.make_path(dir, child);
         const solutions = Json.load(Folder.make_path(dir, 'solutions.json'));
-        if (show) console.error(`${child_dirs[idx]}: ${solutions.toString()}`);
-        all_solutions.push(...solutions);
+        Assert(!result.has(child));
+        result.set(child, solutions);
     }
-    if (show) console.error(`all_solutions: ${all_solutions.toString()}`);
-    return all_solutions;
+    return result;
+}
+
+export const old_show_all = (): void => {
+    const solutions = get_all();
+    for (let key of solutions.keys()) {
+        console.error(`${key}: ${solutions.get(key)!.toString()}`);
+    }
+};
+
+const transform = (file_map: FileMapType): MapType => {
+    let map: MapType = new Map();
+    const outer_keys = Object.keys(file_map);
+    for (const outer_key of outer_keys) {
+        Assert(!map.has(outer_key)); // unexpected dupe, but not surprised if it happens
+        map.set(outer_key, {});
+        const inner_map = file_map[outer_key]!;;
+        const inner_keys = Object.keys(inner_map);
+        for (const inner_key of inner_keys) {
+            const values = inner_map[inner_key]!;
+            if (inner_key !== 'solutions') {
+                // e.g. add "actress" (inner) to "wonder woman" (outer)
+                let obj = map.get(outer_key)!;
+                Assert(!obj.hasOwnProperty(inner_key));
+                obj[inner_key] = values;
+
+                // e.g. add  "wonder woman" (inner) to "actress" (outer)
+                if (!map.has(inner_key)) map.set(inner_key, {});
+                obj = map.get(inner_key)!;
+                if (!obj.hasOwnProperty(outer_key)) obj[outer_key] = [];
+                (obj[outer_key] as string[]).push(...values);
+            }
+            for (const value of values) {
+                if (inner_key === 'solutions') {
+                    Assert(!map.has(value)); // unexpected dupe, but not surprised if it happens
+                    map.set(value, { depends: new Set([outer_key]) });
+                } else { // if (!map.has(value)) {
+                    Assert(!map.has(value)); // unexpected dupe, possibly tricky if it happens
+                    map.set(value, { depends: new Set([outer_key, inner_key]) });
+                }
+            }
+        }
+    }
+    return map;
+};
+
+export const load = (starting_dir: string = process.cwd()): MapType => {
+    const file = Folder.find_file_up(starting_dir, 'solutions.json');
+    return transform(Json.load(file) as FileMapType);
+};
+
+const is_map = (o) => o instanceof Map;
+const is_set = (o) => o instanceof Set;
+
+const my_replacer = (key, value) => {
+    if (is_map(value)) {
+        return { ['M']: Array.from(value) };
+    } else if (is_set(value)) {
+        return { ['S']: Array.from(value) };
+    }
+    return value;
 }
 
 export const show_all = (): void => {
-    get_all(process.cwd(), true);
+    const solutions = load();
+    for (let key of solutions.keys()) {
+        console.error(`${key}: ${/*enhance*/JSON.stringify(solutions.get(key)!, my_replacer)}`);
+    }
 };
